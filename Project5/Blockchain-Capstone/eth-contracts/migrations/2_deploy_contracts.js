04b7cb83786e2a7565e0e3615443d16ff593da64
Le Xuan Hung <lexuanhung020599@gmail.com>
2022-07-17 14:38:01 +0700

    Handle migrate to Rinkeby Testnet

--- Project5/Blockchain-Capstone/eth-contracts/migrations/2_deploy_contracts.js
+++ Project5/Blockchain-Capstone/eth-contracts/migrations/2_deploy_contracts.js
@@ -1,5 +1,6 @@
 // migrating the appropriate contracts
 var Verifier = artifacts.require("./Verifier.sol");
+var BaoHongToken = artifacts.require("./BaoHong.sol");
 var SolnSquareVerifier = artifacts.require("./SolnSquareVerifier.sol");
 
 module.exports = async function(deployer) {
